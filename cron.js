df31bcd9fbe9555c6d3deb190a3015c4f5c9a378
Brendan Nee <brendan.nee@gmail.com>
2013-02-09 23:19:15 -0800

    Handle capitalization

--- cron.js
+++ cron.js
@@ -7,8 +7,10 @@ module.exports = function(app){
   var User = app.set('db').model('user')
     , githubApi = 'https://api.github.com/';
 
-  //update github data on launch
-  updateGithubData();
+  //drop records and update github data on launch
+  User.remove({}, function(){
+    updateGithubData();
+  });
 
   //update github data once per day
   new cronJob('00 42 1 * * *', updateGithubData, null, true);
@@ -16,6 +18,7 @@ module.exports = function(app){
   function updateGithubData () {
     users.forEach(function(batch){
       batch.users.forEach(function(user){
+        user = user.toLowerCase();
         //fetch user info from github
         request.get({
             url: githubApi + 'users/' + user
@@ -26,18 +29,19 @@ module.exports = function(app){
         }, function(e, response, body) {
           if(!body.message) {
             body.batch_id = batch.batch_id;
-            body.login_lower = user.toLowerCase(0);
+            body.login_lower = user.toLowerCase();
             User.update({login_lower: user}, body, {upsert: true}, function(e, count, item){});
+
+            request.get({
+                url: githubApi + 'users/' + user + '/repos'
+              , qs: {
+                    access_token: app.set('githubToken')
+                }
+              , json: true
+            }, function(e, response, body) {
+              User.update({login_lower: user}, {$set: {repos: body}}, function(e, count, item){});
+            });
           }
-        });
-        request.get({
-            url: githubApi + 'users/' + user + '/repos'
-          , qs: {
-                access_token: app.set('githubToken')
-            }
-          , json: true
-        }, function(e, response, body) {
-          User.update({login_lower: user}, {$set: {repos: body}}, function(e, count, item){});
         });
       });
     });
